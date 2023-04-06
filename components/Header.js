fa5ee16de331d66d2d92784d2c4f027cdb15e17a
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-04-06 09:48:33 -0700

    feat(login): hide logged-in elements on login page

--- components/Header.js
+++ components/Header.js
@@ -2,16 +2,29 @@
 
 import { Avatar } from "@mui/material";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import NavigationBar from "./NavigationBar";
 
 export default function Header() {
   const [user, setUser] = useState(null);
+  const pathname = usePathname();
 
   useEffect(() => {
     setUser(JSON.parse(window.localStorage.getItem("user")));
-    console.log({ window });
   }, []);
+
+  if (pathname === "/login") {
+    return (
+      <>
+        <div className="header header-login">
+          <Link href="/" style={{ textDecoration: "none" }}>
+            <h1>feels</h1>
+          </Link>
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
